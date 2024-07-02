f14c2390ad79f4b4ba1a3e89dc0f173337ef3bbc
hwidung <hwhwhw74@naver.com>
2024-07-03 03:52:20 +0900

    design: 아이정보 변경 화면

--- app/src/main/java/com/example/myapplication/Setting/ChildInfoChangeFragment.kt
+++ app/src/main/java/com/example/myapplication/Setting/ChildInfoChangeFragment.kt
@@ -6,17 +6,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.myapplication.R
+import com.example.myapplication.databinding.FragmentChildInfoChangeBinding
 
 
 class ChildInfoChangeFragment : Fragment() {
-
-
+    lateinit var binding: FragmentChildInfoChangeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_child_info_change, container, false)
+        binding=FragmentChildInfoChangeBinding.inflate(inflater,container,false)
+        init()
+        return binding.root
+    }
+
+    private fun init() {
+        TODO( "Not yet implemented")
     }
 
 }
