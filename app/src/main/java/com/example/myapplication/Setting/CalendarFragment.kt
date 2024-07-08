10cc8937a6dbb6e3b4a706199a9a056b3fd46820
hwidung <hwhwhw74@naver.com>
2024-07-08 16:42:57 +0900

    feat: 수정

---
+++ app/src/main/java/com/example/myapplication/Setting/CalendarFragment.kt
@@ -0,0 +1,21 @@
+package com.example.myapplication.Setting
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.myapplication.R
+import com.example.myapplication.databinding.FragmentCalendarBinding
+
+class CalendarFragment : Fragment() {
+    lateinit var binding:FragmentCalendarBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding=FragmentCalendarBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+}
