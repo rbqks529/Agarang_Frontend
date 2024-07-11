4c791fc9f71cbf33b46b275790cca0d7df2460ae
JoGyuBin <rbqls3529@gmail.com>
2024-07-11 15:27:06 +0900

    Feat : date_item 수정

--- app/src/main/java/com/example/myapplication/Diary/CardviewAdapter.kt
+++ app/src/main/java/com/example/myapplication/Diary/CardviewAdapter.kt
@@ -18,13 +18,11 @@ class CardviewAdapter(private var items: List<DiaryMainDayData>) : RecyclerView.
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = items[position]
         holder.binding.apply {
-            tvDate.text = "${item.year}년 ${item.month}월 ${item.day}일"
+            tvDate.text = "${item.year}.${item.month}.${item.day}"
             Glide.with(holder.itemView.context)
                 .load(item.imageResId)
                 .into(ivDiaryImage)
-            tvContent.text = "오늘 엄마랑 대관령에 다녀왔단다. \n" +
-                    "오똑숲에서 많은 나무와 아름다운 할미 꽃을 보며 정말 행복했어. 자연 속에서 느낀 피톤치드 덕분에 마음이 \n" +
-                    "한결 편안해졌단다. 우리 아가도 나중에 꼭 같이 가보자. 사랑해. 우리 아가!" // 실제 데이터로 대체해야 합니다
+            tvContent.text = item.content
         }
     }
 
