739f959f56f8923caae043b365fbd9ab5eeacea5
hwidung <hwhwhw74@naver.com>
2024-07-15 01:32:05 +0900

    feat: 추억공유 기능 폴더 생성, Activity 및 Fragment 초기 세팅

--- app/src/main/java/com/example/myapplication/Memory/PictureFragment.kt
+++ app/src/main/java/com/example/myapplication/Memory/PictureFragment.kt
@@ -7,13 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.myapplication.R
 
-class DeepQuestionFragment : Fragment() {
+
+class PictureFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_deep_question, container, false)
+        return inflater.inflate(R.layout.fragment_picture, container, false)
     }
+
 }
