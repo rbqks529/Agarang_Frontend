980e490a9282f15243ee8cfaff66448c2375e82a
hwidung <hwhwhw74@naver.com>
2024-07-16 13:56:00 +0900

    feat: 심화질문 대화

--- app/src/main/java/com/example/myapplication/Memory/DeepQuestionFragment.kt
+++ app/src/main/java/com/example/myapplication/Memory/DeepQuestionFragment.kt
@@ -6,14 +6,48 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.myapplication.R
+import com.example.myapplication.databinding.FragmentDeepQuestionBinding
 
 class DeepQuestionFragment : Fragment() {
-
+    private lateinit var binding:FragmentDeepQuestionBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_deep_question, container, false)
+        binding= FragmentDeepQuestionBinding.inflate(inflater,container,false)
+        binding.ivRecordBtn.setOnClickListener {
+            binding.ivRecordBtn.visibility=View.GONE
+            binding.tvRecordNotice.visibility=View.VISIBLE
+            binding.ivRecordCancleBtn.visibility=View.VISIBLE
+            binding.ivRecordArrowBtn.visibility=View.VISIBLE
+            binding.ivRecordIng.visibility=View.VISIBLE
+            binding.ivRecordingNextBtn.visibility=View.GONE
+
+        }
+        binding.ivRecordingNextBtn.setOnClickListener {
+            val fragment=SelectInstrumentFragment()
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.memory_frm,fragment)
+                .commit()
+        }
+
+        binding.ivRecordCancleBtn.setOnClickListener {
+            binding.ivRecordBtn.visibility=View.VISIBLE
+            binding.tvRecordNotice.visibility=View.GONE
+            binding.ivRecordCancleBtn.visibility=View.GONE
+            binding.ivRecordArrowBtn.visibility=View.GONE
+            binding.ivRecordIng.visibility=View.GONE
+            binding.ivRecordingNextBtn.visibility=View.GONE
+        }
+
+        binding.ivRecordArrowBtn.setOnClickListener {
+            binding.ivRecordBtn.visibility=View.GONE
+            binding.tvRecordNotice.visibility=View.GONE
+            binding.ivRecordCancleBtn.visibility=View.GONE
+            binding.ivRecordArrowBtn.visibility=View.GONE
+            binding.ivRecordIng.visibility=View.GONE
+            binding.ivRecordingNextBtn.visibility=View.VISIBLE
+        }
+        return binding.root
     }
 }
