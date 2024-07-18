5fb21c3e583d744e2e95e8f2b46098b3772d7bea
juyeon <jjjjyy020704@naver.com>
2024-07-18 21:07:26 +0900

    [Feat]: SelectLoadingFragment구현, retrofit의존성 추가

---
+++ app/src/main/java/com/example/myapplication/Memory/SelectLoadingFragment.kt
@@ -0,0 +1,31 @@
+package com.example.myapplication.Memory
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+import com.example.myapplication.R
+
+
+class SelectLoadingFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_select_loading, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val imageView = view.findViewById<ImageView>(R.id.iv_baby_character)
+        val rotateAnimation = AnimationUtils.loadAnimation(requireContext(), R.anim.rotate_anim)
+        imageView.startAnimation(rotateAnimation)
+    }
+
+}
