8edecc23129847ff8399b49c345fcada878dc32a
juyeon <jjjjyy020704@naver.com>
2024-07-27 17:57:08 +0900

    홈 layout 수정, 음악 앨범 icon 수정

--- app/src/main/java/com/example/myapplication/Memory/MemoryMainActivity.kt
+++ app/src/main/java/com/example/myapplication/Memory/MemoryMainActivity.kt
@@ -11,7 +11,7 @@ class MemoryMainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_memory_main)
 
 
-        val fragment=PicAssociationFragment()
+        val fragment=PictureFragment()
         supportFragmentManager.beginTransaction()
             .replace(R.id.memory_frm,fragment)
             .commit()
