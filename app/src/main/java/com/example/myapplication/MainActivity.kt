36ea3f57ca66023138dc503cbc3214d2cb0ec34f
JoGyuBin <rbqls3529@gmail.com>
2024-07-31 12:08:16 +0900

    Feat : Activity 상태바 색상, style 수정

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -28,8 +28,8 @@ class MainActivity : AppCompatActivity() {
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
                         View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
                         View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-            window.statusBarColor = Color.TRANSPARENT
-            window.navigationBarColor = Color.TRANSPARENT
+            window.statusBarColor = Color.parseColor("#797979")
+            window.navigationBarColor = Color.WHITE
         }
 
         initBottomNavigation()
