90dd2ac0bbc4da3afd3909a5878d009236f5a7b0
hwiwon <hwhwhw74@naver.com>
2024-08-04 16:54:29 +0900

    chore : 로그인 폴더로 이동

--- app/src/main/java/com/example/myapplication/Login/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/Login/LoginActivity.kt
@@ -1,17 +1,18 @@
-package com.example.myapplication
+package com.example.myapplication.Login
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_frm)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
