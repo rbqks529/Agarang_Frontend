f027fa47a70390f7b7ddc05a77bca2e496c5d4c4
juyeon <jjjjyy020704@naver.com>
2024-08-19 22:39:18 +0900

    [FEAT]: 스플래시 화면 구현

---
+++ app/src/main/java/com/example/myapplication/SplashActivity.kt
@@ -0,0 +1,39 @@
+package com.example.myapplication
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+
+        // View에 WindowInsets를 적용
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // DURATION 후에 MainActivity로 이동
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, OnboardingActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+            finish()
+        }, DURATION)
+    }
+
+    companion object {
+        private const val DURATION: Long = 3000 // 3초 대기 시간
+    }
+}
